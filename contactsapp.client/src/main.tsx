c92cbfd65c4d94c5546e11a5e6a2aa13870578c8
jkrupiniewicz <jkrupiniewicz@gmail.com>
2025-04-11 18:48:42 +0200

    Zaktualizowano strukturę aplikacji i zależności
    Zmiana nagłówka HTML oraz tytułu strony na "ContactsApp".
    Dodano `react-router` jako nową zależność oraz zaktualizowano inne pakiety.
    Usunięto logikę prognozy pogody w `App.tsx` i wprowadzono nowe komponenty: `ContactsPage`, `LoginPage`, `NotFoundPage` i `RegisterPage`.
    Zaktualizowano stylizację w `index.css` oraz sposób renderowania w `main.tsx` z użyciem `RouterProvider`.
    Dostosowano `tsconfig.app.json`, aby uwzględnić `main.tsx`.

--- contactsapp.client/src/main.tsx
+++ contactsapp.client/src/main.tsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import LoginPage from "./pages/login";
+import RegisterPage from "./pages/register";
+import NotFoundPage from "./pages/not-found";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginPage />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
---
+++ contactsapp.client/src/pages/contacts.tsx
@@ -0,0 +1,10 @@
+const ContactsPage = () => {
+  return (
+    <div>
+      <h1>Contacts</h1>
+      <p>This is the contacts page.</p>
+    </div>
+  );
+};
+
+export default ContactsPage;
